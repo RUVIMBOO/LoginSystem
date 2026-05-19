161322f75bf65e6a7bde02aa349221181f044a8c
RUVIMBOO <st10459620@myemeris.edu.za>
2026-05-19 19:24:20 +0200

    I added Part 2 of the POE. I also perfected some area that needed it.

--- src/main/java/com/mycompany/loginsystems/LoginSystems.java
+++ src/main/java/com/mycompany/loginsystems/LoginSystems.java
@@ -10,7 +10,10 @@ public class LoginSystems {
 
         Login login = new Login();
 
-        // REGISTER
+        Message msg = new Message();
+
+        // ================= REGISTER =================
+
         System.out.println("=== REGISTER ===");
 
         System.out.print("Enter username: ");
@@ -23,11 +26,14 @@ public class LoginSystems {
         String cell = input.nextLine();
 
         String registerMessage =
-                login.registerUser(username, password, cell);
+                login.registerUser(username,
+                        password,
+                        cell);
 
         System.out.println(registerMessage);
 
-        // LOGIN
+        // ================= LOGIN =================
+
         System.out.println("\n=== LOGIN ===");
 
         System.out.print("Enter username: ");
@@ -37,10 +43,109 @@ public class LoginSystems {
         String loginPass = input.nextLine();
 
         boolean loginStatus =
-                login.loginUser(loginUser, loginPass);
+                login.loginUser(loginUser,
+                        loginPass);
 
         System.out.println(
                 login.returnLoginStatus(loginStatus));
+
+        // ================= PART 2 =================
+
+        if (loginStatus) {
+
+            System.out.println("\nWelcome to QuickChat");
+
+            int option;
+
+            do {
+
+                System.out.println("\n===== MENU =====");
+                System.out.println("1) Send Messages");
+                System.out.println("2) Show recently sent messages");
+                System.out.println("3) Quit");
+
+                System.out.print("Choose option: ");
+
+                option = input.nextInt();
+                input.nextLine();
+
+                // ================= SEND MESSAGE =================
+
+                if (option == 1) {
+
+                    msg.generateMessageID();
+
+                    // Recipient number
+                    System.out.print("Enter recipient number: ");
+                    msg.recipient = input.nextLine();
+
+                    System.out.println(
+                            msg.checkRecipientCell(
+                                    msg.recipient));
+
+                    // Message text
+                    System.out.print("Enter message: ");
+                    msg.message = input.nextLine();
+
+                    // Message length check
+                    if (msg.message.length() <= 250) {
+
+                        System.out.println(
+                                "Message ready to send.");
+
+                        // Create message hash
+                        msg.createMessageHash();
+
+                        // Message options
+                        System.out.println("\n1) Send Message");
+                        System.out.println("2) Discard Message");
+                        System.out.println("3) Store Message");
+
+                        System.out.print("Choose: ");
+
+                        int sendChoice = input.nextInt();
+                        input.nextLine();
+
+                        // Show result
+                        System.out.println(
+                                msg.SentMessage(sendChoice));
+
+                        if (sendChoice == 1) {
+
+                        msg.addMessageToArray();
+                         }
+                        
+                        // Print message details
+                        System.out.println(
+                                msg.printMessages());
+
+                    } else {
+
+                        int extra =
+                                msg.message.length() - 250;
+
+                        System.out.println(
+                                "Message exceeds 250 characters by "
+                                + extra);
+                    }
+
+                }
+
+                // ================= SHOW MESSAGES =================
+
+                else if (option == 2) {
+
+                    System.out.println(
+                            "Coming Soon.");
+                }
+
+            } while (option != 3);
+         System.out.println(
+        "Total messages sent: "
+        + msg.returnTotalMessages());
+         
+            System.out.println("Goodbye.");
+        }
 
         input.close();
     }
